f8ebd96cbafe05630e26434ac174acb298a33e12
Mark Saunders <enfortis.tech@gmail.com>
2020-09-16 13:29:33 -0500

    added image filtering service endpoint.

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import {filterImageFromURL, deleteLocalFilesWithDir} from './util/util';
+import fs from 'fs';
 
 (async () => {
 
@@ -12,6 +13,8 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
+  var imageFileArray = [""];
+  var tempFilePath = "";
 
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
@@ -36,6 +39,47 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
+
+
+  
+
+  // add new filtering microservice endpoint
+  app.get("/filteredimage", async(req, res) => {
+    console.log('hello from c2-image-filter/filteredimage endpoint');
+    console.log(`req params: ${req.query.image_url}`);
+
+    let inputFile = req.query.image_url;
+
+    console.log('calling filterImageFromURL()');
+     
+    var filteredImageFile = "";
+
+    try{
+      filteredImageFile = await filterImageFromURL(inputFile);
+      imageFileArray.push(filteredImageFile);   // save filename into array for later deletion
+      console.log('returned from filterImageFromURL() function');
+
+      // send file back to client in resp.send()
+      res.status(200);
+      //res.send(`returning file ${filteredImageFile}`);
+      //res.send('hello from c2-image-filter/filteredimage endpoint');
+      res.sendFile(filteredImageFile);
+   
+      res.on('finish', () => {
+        console.log('delete file from server.ts ', filteredImageFile);
+        //deleteLocalFiles(imageFileArray);
+        var files = fs.readdirSync('src/util/tmp/');
+        console.log('contents of src/util/tmp ', files);
+        tempFilePath = `${__dirname}`+'/util/tmp/';
+        deleteLocalFilesWithDir(tempFilePath,files);
+
+        console.log('file deletion complete');
+      })
+    } catch(error) {
+      console.log('caught error from filterImageFromURL()', error);
+      res.status(404).send('incorrect file format');
+    }
+  })
   
 
   // Start the Server
